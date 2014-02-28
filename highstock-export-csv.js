1282cf0c368f48ab6fa2ef29b7f448f94ba5e4b7
Dustin Nation <d.r.nation@gmail.com>
2014-02-28 16:13:19 -0600

    Allow for series of differing lengths

--- highstock-export-csv.js
+++ highstock-export-csv.js
@@ -2,20 +2,21 @@
  * A small plugin for getting the CSV of a categorized chart
  */
 (function (Highcharts) {
-    
+
     // Options
     var itemDelimiter = ',',  // use ';' for direct import to Excel
         lineDelimiter = '\n';
-        
+
     var each = Highcharts.each;
     Highcharts.Chart.prototype.getCSV = function () {
         var xAxis = this.xAxis[0],
             columns = [],
             line,
             tempLine,
-            csv = "", 
+            csv = "",
             row,
             col,
+            longestColumn = 0,
             isStock = this.options._stock,
             includeNavigator = (this.options.exporting.hasOwnProperty("csvIncludeNavigator") && this.options.exporting.csvIncludeNavigator),
             convertDates = (this.options.exporting.hasOwnProperty("csvDateAsISO") && this.options.exporting.csvDateAsISO);
@@ -25,6 +26,7 @@
             columns[0].unshift("");
         }
         each (this.series, function (series) {
+            console.log(series.xData.length)
             if(isStock){
                 //Only include the Navigator series if the option exporting.csvIncludeNavigator is true
                 if(series.name != "Navigator" || (includeNavigator && series.name === "Navigator")){
@@ -40,24 +42,38 @@
             }
         });
 
+        //Get the longest column
+        for (col=0; col<columns.length; col++) {
+            if(columns[col].length > longestColumn){
+                longestColumn = columns[col].length;
+            }
+        }
+
         // Transform the columns to CSV
-        for (row = 0; row < columns[0].length; row++) {
+        for (row = 0; row < longestColumn; row++) {
             line = [];
             for (col = 0; col < columns.length; col++) {
-                //If this is a stockchart and the exporting.csvDateAsISO option is true,
-                //convert the date integers to iso date strings
-                if(isStock && convertDates && row>0 && col%2<1){
-                    line.push(new Date(parseInt(columns[col][row])).toISOString());
+                //only do something if this column has data at the current row number
+                if ( columns[col][row] != undefined ){
+                    //If this is a stockchart and the exporting.csvDateAsISO option is true,
+                    //convert the date integers to iso date strings
+                    if(isStock && convertDates && row>0 && col%2<1){
+                        line.push(new Date(parseInt(columns[col][row])).toISOString());
+                    }
+                    else{
+                        line.push(columns[col][row]);
+                    }
                 }
                 else{
-                    line.push(columns[col][row]);
+                    line.push("");
                 }
+
             }
             csv += line.join(itemDelimiter) + lineDelimiter;
         }
 
         return csv;
-        };
+    };
 
     // Now we want to add "Download CSV" to the exporting menu. We post the CSV
     // to a simple PHP script that returns it with a content-type header as a 
@@ -68,11 +84,10 @@
     Highcharts.getOptions().exporting.buttons.contextButton.menuItems.push({
         text: 'Download CSV',
         onclick: function () {
+            this.getCSV();
             Highcharts.post('http://www.highcharts.com/studies/csv-export/csv.php', {
                 csv: this.getCSV()
             });
         }
-    }); 
+    });
 }(Highcharts));
-
-
